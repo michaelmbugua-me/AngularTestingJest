3544cb5a51016cbc8f7c997b7bbb88ee79c95e96
Michael Mbugua <MichaelMbugua.me@gmail.com>
2025-08-15 09:59:34 +0300

    Add a counter service that returns an initial value for the counter

---
+++ src/app/Counter/CounterService.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, delay, Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+
+  getInitialCount(): Observable<number> {
+    return of(10).pipe(delay(1000));
+  }
+
+}
