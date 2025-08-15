fed269a4ebf017565a088cb7669dd5b40747d655
Kelvin Mbugua <kelvinmbugua@Kelvins-MacBook-Air.local>
2025-08-15 03:45:02 +0300

    Set up all testing files

---
+++ setup-jest.ts
@@ -0,0 +1,4 @@
+import 'jest-preset-angular/jest-preset';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv();
---
+++ src/app/Counter/CounterComponent.ts
@@ -0,0 +1,26 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <div>
+      <button (click)="decrement()">-</button>
+      <span>{{ count }}</span>
+      <button (click)="increment()">+</button>
+    </div>
+  `
+})
+export class CounterComponent {
+  @Input() count: number = 0;
+  @Output() countChange = new EventEmitter<number>();
+
+  increment() {
+    this.count++;
+    this.countChange.emit(this.count);
+  }
+
+  decrement() {
+    this.count--;
+    this.countChange.emit(this.count);
+  }
+}
